1d45f25fc8da9c418146aea506e328c7510dc545
alessandrolongo99 <longoalessandro153@gmail.com>
2022-09-05 16:14:03 +0200

    add class Product. Add subclasses RegisteredUser and Food

--- index.php
+++ index.php
@@ -6,32 +6,72 @@ BONUS:
 Alcuni prodotti (es. antipulci) avranno la caratteristica che saranno disponibili solo in un periodo particolare (es. da maggio ad agosto). -->
 
 <?php
+    require_once(__DIR__.'./traits/traits.php');
     class User{
-        protected $name;
-        protected $surname;
-        protected $email;
+        protected $user_name;
+        protected $user_surname;
+        protected $user_email;
 
         function __construct($_name, $_surname, $_email)
         {
-            $this->name = $_name;
-            $this->surname = $_surname;
-            $this->email = $_email;
+            $this->user_name = $_name;
+            $this->user_surname = $_surname;
+            $this->user_email = $_email;
         }
 
-        public function getName(){
-            return $this->name;
+        public function getUserName(){
+            return $this->user_name;
         }
 
-        public function getSurname(){
-            return $this->surname;
+        public function getUserSurname(){
+            return $this->user_surname;
         }
 
-        public function getEmail(){
-            return $this->email;
+        public function getUserEmail(){
+            return $this->user_email;
         }
     };
 
-    $paperino = new User('Paperino', 'Paperis', 'paperoamoroso99@gmail.com');
-    var_dump($paperino);
-?>
+    class RegisteredUser extends User{
+        public function getDiscount(){
+            return 'sconoto utente registrato';
+        }
+    }
+    class Product{
+        protected $product_name;
+        protected $product_code;
+        protected $product_price;
 
+        function __construct($_name, $_code, $_price)
+        {
+            $this->product_name;
+            $this->product_code;
+            $this->product_price;
+        }
+
+        public function getProductName(){
+            return $this->product_name;
+        }
+
+        public function getProductCode(){
+            return $this->product_code;
+        }
+
+        public function getProductPrice(){
+            return $this->product_price;
+        }
+    }
+
+    class Food extends Product{
+        protected $food_type;
+
+        function __construct($_name, $_code, $_price)
+        {
+            parent::__construct($_name, $_code, $_price);
+            $this->food_type;
+        }
+    }
+
+    // $paperino = new RegisteredUser('Paperino', 'Paperis', 'paperoamoroso99@gmail.com');
+    // var_dump($paperino);
+?>
